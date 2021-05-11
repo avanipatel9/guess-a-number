19f9f1f5c09dee06447f220dc77c89dcdf13d568
Avani Patel <anpatel36@yahoo.in>
2021-05-12 00:39:57 +0530

    Confirm button functionality added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,6 +4,7 @@ import { View, Text, TextInput, Button, StyleSheet, TouchableWithoutFeedback, Ke
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 
 const StartGameScreen = props => {
 
@@ -33,12 +34,19 @@ const StartGameScreen = props => {
         setConfirmed(true);
         setSelectedNumber(parseInt(chosenNumber));
         setEnteredValue('');
+        Keyboard.dismiss();
     }
 
     let confirmedOutput;
 
     if(confirmed) {
-        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+        confirmedOutput = 
+        <Card style={styles.summaryContainer}>
+            <Text>You selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="START GAME" />
+        </Card>
+        
     }
 
     return (
@@ -97,6 +105,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 });
 
