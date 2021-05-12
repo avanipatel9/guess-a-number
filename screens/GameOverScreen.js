a5c3936e70ba4e7954252c9207fabf0227578039
Avani Patel <anpatel36@yahoo.in>
2021-05-13 01:32:57 +0530

    Styling using a global style-sheet

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
+
 import GameScreen from './GameScreen';
+import DefaultStyles from '../constants/default-styles';
+
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over!</Text>
+            <Text style={DefaultStyles.title}>The Game is Over!</Text>
             <Text>Number of Rounds: {props.roundsNumber}</Text>
             <Text>Number was: {props.userNumber}</Text>
             <Button title="NEW GAME" onPress={props.onRestart} />
