42278ff63c38f9a03e1d346685d6a607e9d26ec7
Avani Patel <anpatel36@yahoo.in>
2021-05-13 01:20:07 +0530

    Styling using wrapper classes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,8 @@ import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
-
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 const StartGameScreen = props => {
 
     const [enteredValue, setEnteredValue] = useState('');
@@ -42,7 +43,7 @@ const StartGameScreen = props => {
     if(confirmed) {
         confirmedOutput = 
         <Card style={styles.summaryContainer}>
-            <Text>You selected</Text>
+            <BodyText>You selected</BodyText>
             <NumberContainer>{selectedNumber}</NumberContainer>
             <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
         </Card>
@@ -54,9 +55,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss();
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title} >Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input
                         style={styles.input}
                         blurOnSubmit
