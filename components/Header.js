5b4e186b93e51ff2c71c37f04234efdc564cfb60
Avani Patel <anpatel36@yahoo.in>
2021-05-15 18:57:14 +0530

    Using the Platform API

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 
 import Colors from '../constants/colors';
 import TitleText from '../components/TitleText';
@@ -7,7 +7,7 @@ import TitleText from '../components/TitleText';
 const Header = props => {
     return (
         <View style={styles.header}>
-            <TitleText>{props.title}</TitleText>
+            <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     );
 }
@@ -18,9 +18,14 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.primary,
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
+        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : Colors.primary,
+        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    title: {
+        color: Platform.OS === 'ios' ? Colors.primary : 'white'
     }
 });
 
